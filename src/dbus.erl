0329a9e90305632a8b5cd4a3460f084699ba404c
Tony Rogvall <tony@rogvall.se>
2013-02-20 10:10:46 +0100

    added copyright, fixed date

--- src/dbus.erl
+++ src/dbus.erl
@@ -1,5 +1,20 @@
+%%%---- BEGIN COPYRIGHT -------------------------------------------------------
+%%%
+%%% Copyright (C) 2007 - 2013, Rogvall Invest AB, <tony@rogvall.se>
+%%%
+%%% This software is licensed as described in the file COPYRIGHT, which
+%%% you should have received as part of this distribution. The terms
+%%% are also available at http://www.rogvall.se/docs/copyright.txt.
+%%%
+%%% You may opt to use, copy, modify, merge, publish, distribute and/or sell
+%%% copies of the Software, and permit persons to whom the Software is
+%%% furnished to do so, under the terms of the COPYRIGHT file.
+%%%
+%%% This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+%%% KIND, either express or implied.
+%%%
+%%%---- END COPYRIGHT ---------------------------------------------------------
 %%% @author Tony Rogvall <tony@rogvall.se>
-%%% @copyright (C) 2012, Tony Rogvall
 %%% @doc
 %%%     Top level dbus constructs
 %%% @end
@@ -9,36 +24,64 @@
 
 -compile(export_all).
 
+%%
+%% open a dbus_connection
+%% the connection is authenticated and is hello negotiated automatically.
+%% return {ok, Connection} | {error, Reason}
+%%
 open() ->
     dbus_connection:open().
 
 open(Address) ->
     dbus_connection:open(Address).
 
+%%
+%% dbus_compile:generate() is needed !!!
+%%
 
-%% standard method
-ping(Connection) ->
-    dbus_connection:call(Connection,
-			 [{destination,"org.freedesktop.DBus"},
-			  {path,"/org/freedesktop/DBus"},
-			  {interface, "org.freedesktop.DBus.Peer"},
-			  {member,"Ping"}],
-			 "", []).
+%% standard method peer
+ping(C) ->
+    org_freedesktop_dbus_peer:ping(C, []).
 
-get_machine_id(Connection) ->
-    dbus_connection:call(Connection,
-			 [{destination,"org.freedesktop.DBus"},
-			  {path,"/org/freedesktop/DBus"},
-			  {interface, "org.freedesktop.DBus.Peer"},
-			  {member,"GetMachineId"}],
-			 "", []).
+get_machine_id(C) ->
+    org_freedesktop_dbus_peer:get_machineid(C, []).
 
-get_connection_unix_user(Connection, BusName) ->
-    dbus_connection:call(Connection,
-			 [{destination,"org.freedesktop.DBus"},
-			  {path,"/org/freedesktop/DBus"},
-			  {interface, "org.freedesktop.DBus"},
-			  {member,"GetConnectionUnixUser"}],
-			 "s", [BusName]).
+%%
+%% org.freedesktop.DBus 
+%%
+request_name(C,Name,Flags) -> 
+    %% this call has speical treat in dbus_connection. The caller
+    %% (owner process) will get calls and signals sent 
+    org_freedesktop_dbus:list_names(C, [Name,Flags]).
 
+release_name(C,Name) ->
+    %% this call has speical treat in dbus_connection. The name stops
+    %% sending to owner process
+    org_freedesktop_dbus:list_names(C, [Name]).
+
+
+list_names(C) ->
+    org_freedesktop_dbus:list_names(C, []).
+
+list_activatable_names(C) ->
+    org_freedesktop_dbus:list_activatable_names(C, []).
+
+name_has_owner(C,Name) ->
+    org_freedesktop_dbus:name_has_owner(C, [Name]).
     
+
+start_servie_by_name(C,Name,Flags) ->
+    org_freedesktop_dbus:get_name_owner(C, [Name,Flags]).
+
+update_activation_environment(C,Env) ->
+    org_freedesktop_dbus:update_activation_environment(C, [Env]).
+
+get_connection_unix_user(C, BusName) ->
+    org_freedesktop_dbus:get_connection_unix_user(C, [BusName]).
+
+get_connection_unix_process_id(C, BusName) ->
+    org_freedesktop_dbus:get_connection_unix_process_id(C, [BusName]).
+
+get_name_owner(C,Name) ->
+    org_freedesktop_dbus:get_name_owner(C, [Name]).
+
